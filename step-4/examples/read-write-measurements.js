52c6cd4fe3ebc0580bf8e13e09dfba17e8fbad13
Aaron Trippaers <aaron.trippaers@extrahorizon.com>
2023-10-16 15:33:47 +0200

    Update the function to send an email once the measurement is done

---
+++ step-4/examples/read-write-measurements.js
@@ -0,0 +1,27 @@
+const { getSDK } = require('./auth.js');
+const { EOL } = require('os');
+
+const SYSTOLIC = 125;
+const DIASTOLIC = 80;
+
+(async () => {
+  const sdk = await getSDK();
+
+  //Write a new blood pressure document
+  const createdDocument = await sdk.data.documents.create('blood-pressure-measurement', { systolic: SYSTOLIC, diastolic: DIASTOLIC, timestamp: new Date() });
+  console.log('👉 New document created!');
+
+  // Wait 2 seconds to make sure the analysis is done
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  //Read the blood pressure document
+  const retrievedDocument= await sdk.data.documents.findById('blood-pressure-measurement', createdDocument.id);
+  console.log(`👉 Blood Pressure is categorize as ${retrievedDocument.data.category}`);
+
+  //Remove the blood pressure document again
+  const deleted = await sdk.data.documents.remove('blood-pressure-measurement', createdDocument.id);
+
+  if(deleted.affectedRecords === 1) {
+    console.log('👉 Document deleted again');
+  }
+})()
